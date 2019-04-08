60d232d7d888b57a870182bc0b38223c12cc6a71
lettiev <47318127+lettiev@users.noreply.github.com>
2019-04-08 10:32:25 +0200

    Javascript ejercicio 1

---
+++ script.js
@@ -0,0 +1,111 @@
+/*  --- Ejercicio 1 ---
+
+console.log("¡Hola mundo!");
+
+*/
+
+
+/*  --- Ejercicio 2 ---
+
+alert("¡Me llamo Leticia!");
+
+*/
+
+
+/*  --- Ejercicio 3 ---
+
+var nombre = "Leticia";
+var apellido = "Vazquez";
+
+console.log(nombre,apellido);
+
+*/
+
+
+/*  --- Ejercicio 4 ---
+
+var a = 27;
+var b = 23;
+var suma = a + b;
+
+console.log("La suma entre 27 y 23 es: ",suma);
+
+*/
+
+
+/*  --- Ejercicio 5 ---
+
+var nota = 10;
+
+if (nota >= 5) {
+	alert("Has aprobado el exámen con un: " + nota);
+
+} else {
+	alert("Has suspendido el exámen con un: " + nota);
+}
+
+*/
+
+
+/*  --- Ejercicio 6 ---
+
+var color = "azul";
+color = color.replace("azul","verde");
+console.log("Tengo un coche " + color);
+
+var coche = "Tengo un coche azul";
+coche = coche.replace("o","u");
+console.log(coche);
+
+*/
+
+
+/*  --- Ejercicio 7 ---
+
+var objetos = ["mesa","silla","ordenador","libreta"];
+console.log(objetos.length);
+for (var i =0; i<objetos.length; i++) {
+	console.log("El objeto ",objetos[i]," está en la posición ",i + 1);
+}
+
+
+
+
+/*  --- Ejercicio 8 ---
+
+	
+
+function calculadora(operador,valor1,valor2) {
+
+
+
+	if (operador == "suma") {  
+
+		return (valor1 + valor2);
+
+	}
+
+	else if (operador == "resta") {
+
+		return (valor1 - valor2);
+
+		}
+
+	else if (operador == "multiplica") {
+
+		return (valor1 * valor2);
+
+		}
+
+	else if (operador == "divide") {
+
+		return (valor1 / valor2);
+
+		}	
+
+}
+
+var resultado = calculadora("multiplica",100,60);
+
+console.log("El resultado de la operación matemática es: ",resultado);*/
+
